e9f839e9929cc608012e3a3d05bebcefb93beac4
edwinkim97 <edwinkim124@gmail.com>
2022-04-14 15:47:26 -0700

    finished further study for dsa

--- binary-tree.js
+++ binary-tree.js
@@ -51,9 +51,12 @@ class BinaryTree {
    * (i.e. are at the same level but have different parents. ) */
 
   areCousins(node1, node2) {
+    // sets our initial level to 0
     let level = 0;
     let toVisitQueue = [[this.root, level]];
 
+    // handles case where root node is node we are looking for, which means
+    // the two nodes can't be cousins
     if(this.root === node1 || this.root === node2) return false;
 
     let parent1;
@@ -61,27 +64,40 @@ class BinaryTree {
     let parent2;
     let levelNode2;
 
-    while(toVisitQueue){
-      console.log("queue", toVisitQueue);
+    while(toVisitQueue.length > 0){
+      // removing from our queue
       let [currentNode, level] = toVisitQueue.shift();
-      console.log("Current node", currentNode, "level", level)
-      if(currentNode?.left === node1 || currentNode?.right === node1){
+
+      // sets parent1 and levelNode1 to currentNode and currentNode's level + 1 
+      // if either of its children are node 1
+      if(currentNode.left === node1 || currentNode.right === node1){
         parent1 = currentNode;
         levelNode1 = level + 1; 
       }
-      if(currentNode?.left === node2 || currentNode?.right === node2){
+
+      // sets parent2 and levelNode2 to currentNode and currentNode's level + 1 
+      // if either of its children are node 2
+      if(currentNode.left === node2 || currentNode.right === node2){
         parent2 = currentNode;
         levelNode2 = level + 1; 
       }
-      
-      toVisitQueue.push([currentNode?.left, level +1]);
-      toVisitQueue.push([currentNode?.right, level+1]);
+
+      if (parent1 && parent2) {
+        break;
+      }
+
+      // pushing children if they exist
+      if (currentNode.left) {
+        toVisitQueue.push([currentNode.left, level +1]);
+      }
+      if (currentNode.right) {
+        toVisitQueue.push([currentNode.right, level+1]);
+      }
     }
     if(parent1 === parent2 || levelNode1 !== levelNode2){
       return false;
     }
     return true;
-
   }
 
 }
