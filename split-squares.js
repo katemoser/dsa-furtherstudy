668650966955f941c4ff194eeddf0ff372bf47e2
edwinkim97 <edwinkim124@gmail.com>
2022-04-14 17:30:02 -0700

    finished split squares exercise

--- split-squares.js
+++ split-squares.js
@@ -16,12 +16,12 @@ function dump(square) {
 
 /** Validating square's structure */
 function validate(square) {
-// TODO: PROBABLY NEED TRUTH CHECKER ARRAY
+    // TODO: PROBABLY NEED TRUTH CHECKER ARRAY
     if (square === 1 || square === 0) {
         return true;
     }
 
-    if (Array.isArray(square)){
+    if (Array.isArray(square)) {
         if (square.length !== 4) {
             return false;
         }
@@ -41,29 +41,47 @@ function simplify(square) {
         return square;
     }
 
-    if(Array.isArray(square)){
+    if (Array.isArray(square)) {
         let simplifiedSquare = []
-        for(let smallerSquare of square){
+        for (let smallerSquare of square) {
             simplifiedSquare.push(simplify(smallerSquare));
         }
 
-        if(simplifiedSquare.includes(0) && !simplifiedSquare.includes(1)){
+        if (simplifiedSquare.includes(0) && !simplifiedSquare.includes(1)) {
             simplifiedSquare = 0
         }
-        else if(simplifiedSquare.includes(1) && !simplifiedSquare.includes(0)){
+        else if (simplifiedSquare.includes(1) && !simplifiedSquare.includes(0)) {
             simplifiedSquare = 1
         }
         return simplifiedSquare
     }
 }
 
-function add(s1, s2){
-    if(typeof(s1) === "number" && typeof (s2) === "number"){
-        if(s1 ===1 || s2 === 1){
+function add(s1, s2) {
+    if (typeof (s1) === "number" && typeof (s2) === "number") {
+        if (s1 === 1 || s2 === 1) {
             return 1;
         }
         return 0;
     }
+
+    if (Array.isArray(s1) && typeof (s2) === "number") {
+        const expandedSquare = [s2, s2, s2, s2];
+        s2 = expandedSquare;
+    }
+
+    else if (Array.isArray(s2) && typeof (s1) === "number") {
+        const expandedSquare = [s1, s1, s1, s1];
+        s1 = expandedSquare;
+    }
+
+    const resultsArray = [];
+
+    for (let i = 0; i < s1.length; i++) {
+        resultsArray.push(add(s1[i], s2[i]));
+    }
+    return resultsArray;
+
 }
 
 
--- test-squares.js
+++ test-squares.js
@@ -1,9 +1,10 @@
 const {validate, dump, simplify, add} = require("./split-squares")
 
-const square1 = [0, 0, 1, 0];
+const square1 = [0, 1, 1, 0];
 const square2 = [1, 1, [0, 1, 0, 0], 0];
 const square3 = 0;
 const square4 = 1;
+const square5 = [1, 0, 0, 1];
 
 const badSquare1 = 3;
 const badSquare2 = [1, 0, 1];
@@ -13,4 +14,4 @@ const complexSquare1 = [1, 0, [1, [0, 0, 0, 0], 1, [1, 1, 1, 1]], 1];
 const complexSquare2 = [1, 1, 1, [1, 1, 1, 1]];
 const complexSquare3 = [[1, 1, 1, 1], [1, 1, 1, 1], 1, 1];
 
-console.log(add(1,1));
+console.log(add(complexSquare1, square4));
